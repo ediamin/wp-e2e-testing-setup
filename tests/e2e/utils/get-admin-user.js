c6effcf0d29152c5bca0381e4252efabd491619f
Edi Amin <to.ediamin@gmail.com>
2020-06-24 13:34:34 +0600

    Add utils functions

---
+++ tests/e2e/utils/get-admin-user.js
@@ -0,0 +1,10 @@
+import { WP_USERNAME, WP_PASSWORD } from '@wordpress/e2e-test-utils/build/shared/config';
+
+function getAdminUser () {
+	return {
+		username: WP_USERNAME,
+		password: WP_PASSWORD,
+	};
+}
+
+export default getAdminUser;
---
+++ tests/e2e/utils/reset-database.js
@@ -0,0 +1,11 @@
+import initConfig from '@wordpress/env/lib/init-config';
+import { configureWordPress, resetDatabase as resetWPDatabase } from '@wordpress/env/lib/wordpress';
+
+async function resetDatabase() {
+	const config = await initConfig({});
+
+	await resetWPDatabase( 'tests', config );
+	await configureWordPress( 'tests', config );
+}
+
+export default resetDatabase;
